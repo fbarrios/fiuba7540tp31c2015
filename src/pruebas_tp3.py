a4282ce86104242a8eb83a82c29b9ca04e175408
Federico Barrios <fbarrios@fi.uba.ar>
2015-05-11 18:54:14 -0300

    Pruebas boceto.

---
+++ src/pruebas_tp3.py
@@ -0,0 +1,20 @@
+
+
+def correr_pruebas_dados():
+    # Prueba que no se modifiquen las probabilidades si modifico la lista
+    # desde afuera.
+    
+    # Prueba que la probabilidad equiprobable sea equiprobable. (EJ)
+
+
+
+def correr_pruebas_tablero():
+    # Prueba que casilleros no se pueda modificar desde afuera.
+    
+    
+
+
+if __name__ == "__main__":
+    correr_pruebas_dados()
+    correr_pruebas_listado_cartas()
+    correr_pruebas_tablero()
